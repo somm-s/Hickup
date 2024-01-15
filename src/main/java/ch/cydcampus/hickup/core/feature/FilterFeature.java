9acd7c59710eb06e5ee3600942be7222154db83a
Simon Sommerhalder <somm-s@outlook.com>
2024-01-15 10:42:18 +0100

    removed unused parts of project

--- src/main/java/ch/cydcampus/hickup/core/feature/FilterFeature.java
+++
@@ -1,8 +0,0 @@
-package ch.cydcampus.hickup.core.feature;
-
-/*
- * Labels the packet based on a filter that is applied to the packet. All filters are available.
- */
-public class FilterFeature {
-    
-}
