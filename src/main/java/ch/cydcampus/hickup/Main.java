63b4dcf59f4d7699f53d1a017c010cba9e512592
Your Name <you@example.com>
2024-05-28 11:35:40 +0200

    added main class

---
+++ src/main/java/ch/cydcampus/hickup/Main.java
@@ -0,0 +1,15 @@
+package ch.cydcampus.hickup;
+
+
+/**
+ * Main class of the application.
+ * Wraps pipeline and data pre-processing.
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+    }
+
+}
